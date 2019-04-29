2089db74bc2ebb2babe9b84b19474f67ab5a8dff
Thomas Rudin <thomas@rudin.io>
2019-04-29 18:59:47 +0200

    working sounds

---
+++ init.lua
@@ -0,0 +1,21 @@
+
+soundblock.register({
+  filename = "soundblock_scifi_space_engine",
+  key = "soundblock_scifi_space_engine",
+  name = "Scifi sound engine 1"
+})
+
+soundblock.register({
+  filename = "soundblock_scifi_space_engine_2",
+  key = "soundblock_scifi_space_engine_2",
+  name = "Scifi sound engine 2"
+})
+
+soundblock.register({
+  filename = "soundblock_scifi_space_engine_3",
+  key = "soundblock_scifi_space_engine_3",
+  name = "Scifi sound engine 3"
+})
+
+
+print("[OK] soundblock_scifi")
---
+++ readme.md
@@ -0,0 +1,10 @@
+
+
+* soundblock_scifi_space_engine
+* https://freesound.org/people/rhodesmas/sounds/321723/
+
+* soundblock_scifi_space_engine_2
+* https://freesound.org/people/nick121087/sounds/234316/
+
+* soundblock_scifi_space_engine_3
+* https://freesound.org/people/Erdie/sounds/158729/
