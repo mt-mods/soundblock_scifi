fa4171cd9849252039a202470e2d872814dbf1bf
BuckarooBanzay <BuckarooBanzay@github.com>
2019-10-07 08:42:58 +0200

    add lengths

--- init.lua
+++ init.lua
@@ -2,31 +2,36 @@
 soundblock.register({
   filename = "soundblock_scifi_space_engine",
   key = "soundblock_scifi_space_engine",
-  name = "Scifi sound engine 1"
+  name = "Scifi sound engine 1",
+  length = 26
 })
 
 soundblock.register({
   filename = "soundblock_scifi_space_engine_2",
   key = "soundblock_scifi_space_engine_2",
-  name = "Scifi sound engine 2"
+  name = "Scifi sound engine 2",
+  length = 42
 })
 
 soundblock.register({
   filename = "soundblock_scifi_space_engine_3",
   key = "soundblock_scifi_space_engine_3",
-  name = "Scifi sound engine 3"
+  name = "Scifi sound engine 3",
+  length = 24
 })
 
 soundblock.register({
   filename = "soundblock_scifi_ambient_1",
   key = "soundblock_scifi_ambient_1",
-  name = "Scifi sound ambient 1"
+  name = "Scifi sound ambient 1",
+  length = 17
 })
 
 soundblock.register({
   filename = "soundblock_scifi_ambient_2",
   key = "soundblock_scifi_ambient_2",
-  name = "Scifi sound ambient 2"
+  name = "Scifi sound ambient 2",
+  length = 60
 })
 
 
